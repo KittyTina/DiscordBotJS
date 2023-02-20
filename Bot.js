97eda637e948c2a1c0aee8e6d8fe1e1da29234e8
Tina.Hpr <elharper1234@gmail.com>
2023-02-20 23:37:16 +0100

    Greeting a joined user

--- Bot.js
+++ Bot.js
@@ -1,6 +1,9 @@
 const Discord = require('discord.js');
 const config = require("./stuff/config.json");
 const client = new Discord.Client();
+const color_purple = "#644099";
+
+
 client.login(config.token);
 
 
@@ -12,4 +15,12 @@ client.on('message', msg => {
   if (msg.content === '!ping') {
     msg.reply('Ready to serve!');
   }
+});
+
+client.on('guildMemberAdd', member => {
+    const embed = new Discord.MessageEmbed()
+        .setTitle("Welcome, ${member.user.username}")
+        .setDescription('Welcome on the test server!')
+        .setColor(color_purple)
+    member.guild.channels.cache.find(channel => channel.name === 'general').send(embed);
 });
